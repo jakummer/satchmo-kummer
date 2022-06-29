9c497625c1989f6279d890f9e33524e5d0563e7c
jakummer <jakummer@gmail.com>
2022-06-28 21:00:12 -0300

    Menu e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+ 
+
+function NavBar(){
+return(
+       
+        <nav>
+                
+                <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Quienes somos</a></li>
+                        <li><a href="#">Tienda</a></li>
+                        <li><a href="#">Recursos</a></li>
+                        <li><a href="#">Contacto</a></li>
+                </ul>
+        </nav>
+
+        );
+};
+
+export default NavBar;
+
+
+       
+    
